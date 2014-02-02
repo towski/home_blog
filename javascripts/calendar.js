1ee61f1cefed3fa3c7412d585950a6f6cd8c93e1
Matthew Wastrodowski <towski@towski-tek.co>
2014-02-02 12:46:38 -0800

    last commit before date reversal

--- javascripts/calendar.js
+++ javascripts/calendar.js
@@ -5,7 +5,7 @@ function pad(num, size) {
 
 function request_call(td, i, next_date){
 	var request = new XMLHttpRequest()
-	var url = '/journals/2014'.concat(pad((next_date.getMonth() + 1), 2), next_date.getDate(), '.html')
+	var url = '/journals/2014'.concat(pad((next_date.getMonth() + 1), 2), pad(next_date.getDate(), 2), '.html')
 	request.open('head', url) 
 	request.onload = function() {
 		if (this.status == 200 || this.status == 304){
@@ -17,36 +17,43 @@ function request_call(td, i, next_date){
 	request.send()
 }
 
-function build_calendar(){
+function build_calendar(month, year){
 	var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
 	var today = new Date()
-	var month_name_header = document.getElementById('month_name')
-	var month_table = document.getElementById('month')
-	month_name_header.innerHTML = monthNames[today.getMonth()] 
+	var first_day = new Date(year, month - 1, 1)
+	var new_element = document.createElement('div')
+	var month_name_header = document.createElement('h4')
+	new_element.appendChild(month_name_header)
+	var month_table = document.createElement('table')
+	new_element.appendChild(month_table)
+	month_name_header.innerHTML = monthNames[first_day.getMonth()] 
 	var i = 1
-	var first_day = new Date(today.getFullYear(), today.getMonth(), 1)
 	var day = 0
 	var first_row = document.createElement('tr')
 	while(day < first_day.getDay()){
 		var td = document.createElement('td')
 		td.innerHTML = ""
-		td.className = "month_day"
+		td.className = "blank_day"
 		first_row.appendChild(td)
 		day += 1
 	}
 	month_table.appendChild(first_row)
-	var blank_day = new Date(today.getFullYear(), today.getMonth(), 0)
+	var blank_day = new Date(first_day.getFullYear(), first_day.getMonth(), 0)
 	var next_row = first_row
 	while(i <= blank_day.getDate()){
-		var next_date = new Date(today.getFullYear(), today.getMonth(), i)
+		var next_date = new Date(first_day.getFullYear(), first_day.getMonth(), i)
+		if(today.getMonth() == next_date.getMonth() && next_date.getDate() > today.getDate()){
+			break;
+		}
 		var td = document.createElement('td')
 		request_call(td, i, next_date)
 		if (next_date.getDay() == 0){
 			next_row = document.createElement('tr')
 			month_table.appendChild(next_row)
 		}
-		if (next_date.getDate() == today.getDate()){
-			td.className = "month_day today"
+		if (next_date.getDate() == today.getDate() && next_date.getMonth() == next_date.getMonth()){
+			//td.className = "month_day today"
+			td.className = "month_day"
 		} else {
 			td.className = "month_day"
 		}
@@ -54,6 +61,11 @@ function build_calendar(){
 		month_table
 		i += 1
 	}
+	return new_element;
 }
 
-build_calendar()
+var tr = document.getElementById('month_td')
+var new_element = build_calendar(2, 2014)
+tr.appendChild(new_element)
+new_element = build_calendar(1, 2014)
+tr.appendChild(new_element)
